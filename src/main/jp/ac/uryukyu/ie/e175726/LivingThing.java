5eda5fcc8a6a29a77e2d887dd4598071aa9cfe3b
Seiryu Hidaka <e175726@ie.u-ryukyu.ac.jp>
2017-11-12 00:20:14 +0900

    level3 private and getter setter

--- src/main/jp/ac/uryukyu/ie/e175726/LivingThing.java
+++ src/main/jp/ac/uryukyu/ie/e175726/LivingThing.java
@@ -1,10 +1,10 @@
 package jp.ac.uryukyu.ie.e175726;
 
 public class LivingThing {
-    String name;
-    int hitPoint;
-    int attack;
-    boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -28,9 +28,15 @@ public class LivingThing {
         return  dead;
     }
 
+    public void setDead(boolean dead){this.dead=dead;}
+
     public String getName(){
         return name;
     }
+
+    public int getHitPoint(){return hitPoint;}
+
+    public void setHitPoint(int hitPoint){this.hitPoint=hitPoint;}
 
     /**
      * 攻撃するメソッド。
--- src/test/jp/ac/uryukyu/ie/e175726/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e175726/EnemyTest.java
@@ -7,14 +7,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class EnemyTest {
     @Test
     void attack() {
-        int heroHP = 10;
-        Hero hero = new Hero("テスト勇者", heroHP, 5);
-        Enemy enemy = new Enemy("テストスライム", 6, 3);
-        enemy.dead = true;
-        for(int i=0; i<10; i++) {
-            enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
-        }
-        assertEquals(heroHP, hero.hitPoint);
+            int heroHP = 10;
+            Hero hero = new Hero("テスト勇者", heroHP, 5);
+            Enemy enemy = new Enemy("テストスライム", 6, 3);
+            enemy.setDead(true);
+            for (int i = 0; i < 10; i++) {
+                enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
+            }
+            assertEquals(heroHP, hero.getHitPoint());
     }
 
 }
